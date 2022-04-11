04deb1b1153db73560acc30f76b439c7a49f5e30
angelina-tsuboi <angelina.t1832@gmail.com>
2022-04-10 19:12:09 -0700

    bootstrap kind of working

--- AQ_Device.ino
+++ AQ_Device.ino
@@ -63,8 +63,8 @@ const char index_html[] PROGMEM = R"rawliteral(
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
   <!-- Template Main CSS File -->
   <style>
   body {
@@ -402,7 +402,9 @@ h1, h2, h3, h4, h5, h6 {
       </div>
     </section>
   </main><!-- End #main -->
-  
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.0/apexcharts.min.js"></script>
+
 <script>
 if (!!window.EventSource) {
  var source = new EventSource('/events');
